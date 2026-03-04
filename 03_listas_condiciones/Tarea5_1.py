3a8c8699e211d060729cf33761fa94b6caae999c
Corderowski <dereckcb10052004@gmail.com>
2026-03-03 21:14:08 -0600

    Tarea hecha 5_1

--- 03_listas_condiciones/Tarea5_1.py
+++ 03_listas_condiciones/Tarea5_1.py
@@ -1,5 +1,22 @@
+
+#Lista de notas
+lista_notas = []
+
+with open("notas_ejemplo.csv", encoding="utf-8") as archivo:
+    next(archivo)  # Saltar encabezado (nombre,nota)
+    
+    for linea in archivo:
+        datos = linea.strip().split(",")
+        nombre = datos[0]
+        nota = float(datos[1])
+        
+        lista_notas.append(nota)
+
+promedio = sum(lista_notas) / len(lista_notas)
+
+
+
 def clasificar_nota(nota):
-    """Retorna la categoría de la nota."""
     if nota >= 90:
         return "Excelente"
     elif nota >= 75:
@@ -9,20 +26,46 @@ def clasificar_nota(nota):
     else:
         return "Reprobado"
 
+def reporte_salon(lista_notas):
+    promedio = sum(lista_notas) / len(lista_notas)
+    
+aprobados = 0
+reprobados = 0
+eximidos = 0
+    
+for nota in lista_notas:
+    if nota >=90:
+        eximidos += 1
+    elif nota >= 70:
+        aprobados += 1
+    else:
+        reprobados += 1 
+    
 
-# Usar la función con varias notas
-suma  = 0
-total = 0
-with open("H:\Universidad\Github\fundamentos_python\03_listas_condiciones\notas_ejemplo.csv") as archivo:
-    next(archivo) 
-    for linea in archivo:
-        partes = linea.strip().split(",")
-        nota   = float(partes[1])
-        suma  += nota
-        total += 1
+while True:
+    print("\n--- Menú de Notas ---")
+    print("1. Promedio")
+    print("2. Eximidos")
+    print("3. Nota mas alta")
+    print("4. Nota mas baja")
+    print("5. Salir")
 
-print(f"Promedio del salón (desde archivo): {suma/total:.2f}")
+    opcion = input("Opción: ")
 
-for nota in notas_prueba:
-    categoria = clasificar_nota(nota)
-    print(f"Nota {nota} → {categoria}")
+    if opcion == "1":
+        print(f"Los estudiantes presentan un promedio de: {promedio:.2f}")
+    elif opcion == "2":
+        print(f"Cantidad de estudiantes eximidos: {eximidos:}")
+    elif opcion == "3":
+        nota_alta = max(lista_notas)
+        print(f"El estudiante con la nota mas alta fue {nombre} con una nota de {nota_alta}")
+    elif opcion == "4":
+        nota_baja = min(lista_notas)
+        print(f"El estudiante con la nota mas baja fue {nombre} con una nota de {nota_baja}")
+    
+    elif opcion == "5":
+        print("Cerrando programa...")
+        break
+
+    else:
+        print("opcion invalida, intente de nuevo!")
